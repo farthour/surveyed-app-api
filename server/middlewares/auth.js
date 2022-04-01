0d1fbdbe1358e4c47d4dd7ca3508f7c36ffb0c16
Amit Dimri <dimriamit98@gmail.com>
2022-04-01 18:45:31 +0530

    added additional check for refreshToken

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,6 @@
 const httpStatus = require("http-status");
 const passport = require("passport");
+const cookie = require("cookie");
 
 const { roleRights } = require("../config/roles");
 const ApiError = require("../utils/ApiError");
@@ -8,11 +9,22 @@ const authMessages = require("../config/messages").auth;
 const verifyCallback =
   (req, resolve, reject, requiredRights = [], predicate) =>
   async (err, user) => {
-    if (err || !user) {
+    // if there is no refresh token and
+    // req is given a stolen accessToken
+    // then it will successfully execute
+    // So, we are also checking cookie
+    // for refreshTokens
+    const cookies = req.headers.cookie && cookie.parse(req.headers.cookie);
+
+    if (err || !user || !cookies.refreshToken) {
       return reject(
-        new ApiError(httpStatus.UNAUTHORIZED, authMessages.error.pleaseAuthenticate)
+        new ApiError(
+          httpStatus.UNAUTHORIZED,
+          authMessages.error.pleaseAuthenticate
+        )
       );
     }
+
     req.user = user;
 
     // Consider short circuiting this logic for any admin type once FE has
@@ -23,14 +35,18 @@ const verifyCallback =
         userRights.includes(requiredRight)
       );
       if (!hasRequiredRights && req.params.userId !== user.id) {
-        return reject(new ApiError(httpStatus.FORBIDDEN, authMessages.error.forbidden));
+        return reject(
+          new ApiError(httpStatus.FORBIDDEN, authMessages.error.forbidden)
+        );
       }
     }
 
     if (predicate) {
       const predicateResult = await predicate(user, req);
       if (!predicateResult) {
-        return reject(new ApiError(httpStatus.FORBIDDEN, authMessages.error.forbidden));
+        return reject(
+          new ApiError(httpStatus.FORBIDDEN, authMessages.error.forbidden)
+        );
       }
     }
 
